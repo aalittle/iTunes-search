c5c31e47a4177fcaa9569f3157ace7406286196f
Andrew Little <aalittle@gmail.com>
2013-04-20 22:00:48 -0300

    made the site more responsive and centered the search box

--- README.md
+++ README.md
@@ -17,3 +17,16 @@ This application demonstrates the performance of the iTunes Search API for the p
 - Mac DMG [here][node-mac]
 - Windows download from [here][node-windows]. (You will also need [7 Zip] to unzip the node archive)
   (and don't forget to add `node.exe` to  your executable path)
+  
+### AngularJS
+- A good place to learn about Angular is [here][http://angularjs.org/] and note that we also started the project with the [Angular-Seed project][https://github.com/angular/angular-seed]
+
+### Setting up
+
+1. Clone the repository to your local machine.
+	<pre><code>git clone https://github.com/aalittle/iTunes-search.git</code></pre>
+
+2. Change directories into the cloned repository.
+    <pre><code>cd iTunes-search</code></pre>
+
+3. To run it locally, simply execute
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,11 +8,19 @@ function TypeaheadCtrl($scope, $http, limitToFilter) {
 	$scope.radioMedia = 'album' //set a default for the radio button
 
 	//trigger a call to the iTunes Search API with the current albumName in the search box	
-    $scope.albums = function(searchTerm) {
+  $scope.albums = function(searchTerm) {
 	
-	return $http.jsonp("https://itunes.apple.com/search?callback=JSON_CALLBACK&entity=" + $scope.radioMedia + "&limit=10&term="+searchTerm).then(function(response){
-      return limitToFilter(response.data.results, 10);
+	return $http.jsonp("https://itunes.apple.com/search?callback=JSON_CALLBACK&entity=" + $scope.radioMedia + "&limit=20&term="+searchTerm).then(function(response){
+      return limitToFilter(response.data.results, 20);
     });
 	
   };
 }
+
+$('.typeahead').typeahead({
+    updater: function(item) {
+        // do what you want with the item here
+				console.log("fuck");
+        return item;
+    }
+})
